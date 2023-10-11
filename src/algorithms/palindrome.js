7d77793a889f310b1842f8f2abd937dd7f1a5cc5
Semen Ishinov <ectoplazm@gmail.com>
2023-10-11 23:26:46 +0300

    fix code palindrome

--- src/algorithms/palindrome.js
+++ src/algorithms/palindrome.js
@@ -1,7 +1,7 @@
-const isPalindrome = (word) => {
-  for(let i = Math.floor(word.length/2) - 1; i >= 0; i-- ) {
-    let a = word[i]
-    let b = word[word.length - i - 1]
+const isPalindrome = word => {
+  for (let i = Math.floor(word.length / 2) - 1; i >= 0; i--) {
+    const a = word[i]
+    const b = word[word.length - i - 1]
     if (a !== b) return false
   }
 
