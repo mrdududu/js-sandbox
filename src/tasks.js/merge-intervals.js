0e3563bdc6b2e723ea938c8ca0ff1ac7fd6dee81
Semen Ishinov <ectoplazm@gmail.com>
2023-10-13 15:35:52 +0300

    Save

--- src/tasks.js/merge-intervals.js
+++ src/tasks.js/merge-intervals.js
@@ -3,6 +3,12 @@
 const arr = [[1, 3], [2,6], [8, 10], [15, 18]]
 
 function mergeIntervals(inArr) {
+  let li = 0
+  let ri = inArr.length - 1
+
+  // while(li <= ri) {
+  //   let lpair = inArr[li]
+  // }
 
 }
 
