ca48221df3128c3181a64a6f115a138b710867cb
Semen Ishinov <ectoplazm@gmail.com>
2023-10-20 17:42:54 +0300

    Save

---
+++ src/codewars/RomanNumerals.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/51b66044bce5799a7f000003/train/javascript
+
+/**
+to roman:
+2000 -> "MM"
+1666 -> "MDCLXVI"
+1000 -> "M"
+ 400 -> "CD"
+  90 -> "XC"
+  40 -> "XL"
+   1 -> "I"
+
+from roman:
+"MM"      -> 2000
+"MDCLXVI" -> 1666
+"M"       -> 1000
+"CD"      -> 400
+"XC"      -> 90
+"XL"      -> 40
+"I"       -> 1
+ */
+
+class RomanNumerals {
+  static toRoman(num) {
+    return 'IV';
+  }
+
+  static fromRoman(str) {
+    return 4;
+  }
+}
